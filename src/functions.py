7c45ffeac76c28dd6a5e9ba9ed1e72e39d6bbf65
Andrew Krygin <andrew_krygin@protonmail.com>
2025-04-29 12:47:41 +0300

    Updated functions and test_functions files.

--- src/functions.py
+++ src/functions.py
@@ -3,3 +3,19 @@ from typing import Any
 def revers_list(lst: list[Any]) -> list[Any]:
     """Функция переворачивает список."""
     return lst[::-1]
+
+def reverse_string(string: str) -> str:
+    """Функция переворачивает строку."""
+    return string[::-1]
+
+
+def func(lst: list[Any], my_type: type) -> int:
+    if isinstance(lst, list):
+        return len([x for x in lst if isinstance(x, my_type)])
+    raise TypeError("Ошибка: должен быть список!")
+
+
+def max_num(lst: list[int | float]) -> int | float:
+    if isinstance(lst, list):
+        return max(lst)
+    raise TypeError("Должен быть список!")
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,8 +1,30 @@
-from src.functions import revers_list
+import pytest
+
+
+from src.functions import revers_list, reverse_string, func
 
 
 def test_reverse_list(number_list):
     assert revers_list(number_list) == [5, 4, 3, 2, 1]
 
+
 def test_reverse_list_empty():
     assert revers_list([]) == []
+
+
+@pytest.mark.parametrize("string, expected_result", [
+    ("hello", "olleh"),
+    ("world", "dlrow"),
+    ("12345", "54321"),
+    ("", ""),
+])
+def test_reverse_string(string, expected_result):
+    assert reverse_string(string) == expected_result
+
+
+def test_func():
+    assert func([1, 2, 3, [3, 4], {}, (4,), "12", "ghb"], int) == 3
+    assert func([], str) == 0
+    assert func(["1232213"], float) == 0
+    with pytest.raises(TypeError):
+        func(123, str)
